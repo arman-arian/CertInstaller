1a1f6b9c959076477730b9f91aae3cef2478bbf9
arman_arian <arman.hassanpoor2000@gmail.com>
2015-08-19 18:54:28 +0430

    initial commit

---
+++ CertInstaller/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CertInstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var certificate = new X509Certificate2(Certificates.QTasCert, "12345");
+            var rootStore = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
+            rootStore.Open(OpenFlags.ReadWrite);
+            rootStore.Add(certificate);
+            rootStore.Close();
+
+            var myStore = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            myStore.Open(OpenFlags.ReadWrite);
+            myStore.Add(certificate);
+            myStore.Close();
+
+            Console.WriteLine("Certificate Installed.");
+            Console.ReadKey();
+        }
+    }
+}
